f532b11e42c6702c71ecc4a4e295b285c26905a5
hungsodo109@gmail.com <hungsodo109@gmail.com>
2023-02-08 17:56:02 +0700

    - demo front-end

---
+++ TextEx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Display
+{
+    public partial class TextEx : Label
+    {
+        public int SetSpeed
+        {
+            get { return speed; }
+            set { speed = value; Invalidate(); }
+        }
+
+        Timer tmrTick;
+        int position, speed, height, maxPosition;
+        bool enableScrollText = false;
+
+        public TextEx()
+        {
+            InitializeComponent();
+
+            UseCompatibleTextRendering = true;
+
+            tmrTick = new Timer();
+            tmrTick.Tick += tick;
+            tmrTick.Interval = 20;
+            tmrTick.Start();
+        }
+
+        public void Start()
+        {
+            enableScrollText = true;
+            height = this.Size.Height;
+            maxPosition = (int)this.CreateGraphics().MeasureString(this.Text, this.Font, this.Width).Height;
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            maxPosition = (int)this.CreateGraphics().MeasureString(this.Text, this.Font, this.Width).Height;
+
+            base.OnTextChanged(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(0, (float)position);
+
+            base.OnPaint(e);
+        }
+
+        private void tick(object sender, EventArgs e)
+        {
+            if (!enableScrollText) return;
+
+            if (position < -maxPosition)
+            {
+                this.Size = new Size(Width, height);
+                position = height;                
+            }
+
+            position -= speed;
+            Height += speed;
+            Invalidate();
+        }
+    }
+}
---
+++ frmMain.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Display
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtThongBao = new Display.TextEx();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtVanBan = new Display.TextEx();
+            this.tick = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.timer_GetRTC = new System.Windows.Forms.Timer(this.components);
+            this.lbTime = new System.Windows.Forms.Label();
+            this.guna2DragControl2 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2DragControl3 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(459, 180);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(569, 508);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtThongBao);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1016, 123);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông báo";
+            // 
+            // txtThongBao
+            // 
+            this.txtThongBao.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThongBao.ForeColor = System.Drawing.Color.DarkRed;
+            this.txtThongBao.Location = new System.Drawing.Point(3, 22);
+            this.txtThongBao.Name = "txtThongBao";
+            this.txtThongBao.SetSpeed = 1;
+            this.txtThongBao.Size = new System.Drawing.Size(986, 88);
+            this.txtThongBao.TabIndex = 1;
+            this.txtThongBao.Text = resources.GetString("txtThongBao.Text");
+            this.txtThongBao.UseCompatibleTextRendering = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtVanBan);
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 170);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(441, 518);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Văn bản";
+            // 
+            // txtVanBan
+            // 
+            this.txtVanBan.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVanBan.ForeColor = System.Drawing.Color.Black;
+            this.txtVanBan.Location = new System.Drawing.Point(7, 21);
+            this.txtVanBan.Name = "txtVanBan";
+            this.txtVanBan.SetSpeed = 1;
+            this.txtVanBan.Size = new System.Drawing.Size(428, 479);
+            this.txtVanBan.TabIndex = 0;
+            this.txtVanBan.Text = resources.GetString("txtVanBan.Text");
+            this.txtVanBan.UseCompatibleTextRendering = true;
+            // 
+            // tick
+            // 
+            this.tick.Enabled = true;
+            this.tick.Interval = 1000;
+            this.tick.Tick += new System.EventHandler(this.tick_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(50)))), ((int)(((byte)(70)))));
+            this.panel1.Controls.Add(this.lbTime);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1040, 37);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(51, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bảng điện tử";
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 22;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Display.Properties.Resources.vinmec_icon;
+            this.pictureBox2.Location = new System.Drawing.Point(14, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.TargetControl = this.panel1;
+            // 
+            // timer_GetRTC
+            // 
+            this.timer_GetRTC.Interval = 1000;
+            this.timer_GetRTC.Tick += new System.EventHandler(this.timer_GetRTC_Tick);
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.ForeColor = System.Drawing.Color.White;
+            this.lbTime.Location = new System.Drawing.Point(742, 9);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(85, 19);
+            this.lbTime.TabIndex = 4;
+            this.lbTime.Text = "Thời gian";
+            // 
+            // guna2DragControl2
+            // 
+            this.guna2DragControl2.TargetControl = this.label1;
+            // 
+            // guna2DragControl3
+            // 
+            this.guna2DragControl3.TargetControl = this.lbTime;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1040, 700);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bảng hiển thị";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private TextEx txtThongBao;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private TextEx txtVanBan;
+        private System.Windows.Forms.Timer tick;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private System.Windows.Forms.Timer timer_GetRTC;
+        private System.Windows.Forms.Label lbTime;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl2;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl3;
+    }
+}
+
